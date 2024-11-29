57513b3a7fe1dc230432296ad289a945880ee220
Danny McCarragher <dannymccarragher@gmail.com>
2024-11-28 18:01:56 -0800

    Complete Deleting tasks. started on editing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mariadb = require('mariadb');
+const methodOverride = require('method-override');
+
 
 const app = express();
 
@@ -10,6 +12,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
+
+// https://expressjs.com/en/resources/middleware/method-override.html
+app.use(methodOverride('_method'));
 
 const pool = mariadb.createPool({
     host: 'localhost',
@@ -46,23 +51,58 @@ app.post('/submit', async (req, res) => {
                 VALUES ('${data.assignment}','${data.description}', 
                 '${data.class}', '${data.priority}', '${data.date}')`);
 
+    //const assignment_num = data.insertId;
+
+
     res.render('confirmation', {data : data})
 });
 
 app.get('/submit', async (req,res) => {
+    res.render('confirmation', {data : []});
+    
+});
+
+app.get('/tasks', async (req, res) => {
     const conn = await connect();
-    const data = req.body;
-    res.render('confirmation', {data : []})
+    const data = await conn.query('SELECT * FROM assignments');
+    res.render('tasks', { data: data });
 });
 
-app.get('/adminlogin', (req,res)=> {
-    res.render('admin-login');
+// app.post('/adminpage', async (req,res)=> {
+//     const conn = await connect();
+//     const data = await conn.query('SELECT * FROM assignments');
+//     res.render('admin-page', {data : data});
+// });
+
+app.delete('/tasks/:assignment_num', async (req, res) => {
+    const conn = await connect();
+    const assignment_num = req.body.assignment_num;
+
+    await conn.query(`DELETE FROM assignments WHERE assignment_num = ${assignment_num}`);
+    res.redirect('/tasks');
 });
 
-app.get('/adminpage', (req,res)=> {
-    res.render('admin-page');
+app.put('/tasks/:assignment_num', async (req, res) => {
+    const conn = await connect();
+    //const { assignment, description, class: className, priority, date } = req.body;
+    const assignment_num = req.body.assignment_num;
+
+    await conn.query(
+        `UPDATE assignments 
+        SET assignment = '${data.assignment}', 
+            description = '${data.description}', 
+            class = '${data.class}', 
+            priority = '${data.priority}', 
+            date = '${data.date}' 
+        WHERE assignment_num = '${assignment_num}'`
+   );
+   res.redirect('/adminpage');
+
+});
+
+app.get('/assignmentedit', (req,res) => {
+    res.render('assignment-edit');
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
